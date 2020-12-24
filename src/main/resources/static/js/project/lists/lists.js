d3b7b66c47299b04bf82e74066acb254ba45b3b9
xuguangxing <1832377033@qq.com>
2020-12-24 08:27:36 +0800

    提交

--- src/main/resources/static/js/project/lists/lists.js
+++ src/main/resources/static/js/project/lists/lists.js
@@ -15,6 +15,7 @@ layui.config({
     var laytpl = layui.laytpl;
     var util = layui.util;
     var flow = layui.flow;
+    var  layedit = layui.layedit;
 
     let loadIndex = layer.load(0, {
         shade: [0.3, '#333']
@@ -147,6 +148,47 @@ layui.config({
             }
         });
     }
+
+
+    //用户评论
+    layer.photos({
+        photos: '#layer-photos-demo'
+        ,anim: 5
+    });
+
+    $(document).ready(function(){
+        //alert($(".comment_list").find(".comment")[0] == undefined);
+        if($(".comment_list").find(".comment")[0] == undefined) {
+            $(".comment_add_or_last").html("暂无人评论");
+        }
+        $(document).on('click','.show_reply_list', function(){
+
+            if($(this).closest('.comment').find(".reply_list").css("display") != "none") {
+                $(this).closest('.comment').find(".show_remain_reply").css("display","none");
+                $(this).html("查看回复");
+
+            }else {
+                $(this).closest('.comment').find(".show_remain_reply").css("display","block");
+                $(this).html("收起回复");
+
+            }
+            $(this).closest('.comment').find(".reply_list").toggle(500);
+
+
+        });
+
+        $(document).on('click','.show_remain_reply', function(){
+            $(this).html("已显示全部回复");
+            //TODO发送数据
+
+        });
+        $(document).on('click','.comment_add_or_last', function(){
+            var getMoreComment = $(this);
+            //TODO如果获取的数据为零
+            getMoreComment.html("没有更多评论了");
+        });
+
+    });
 
 });
 
