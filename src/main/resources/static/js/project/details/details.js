f9dac3066d5fb5ca45e2b2440fc9845129079f2f
xuguangxing <1832377033@qq.com>
2020-12-26 11:19:51 +0800

    王菲

--- src/main/resources/static/js/project/details/details.js
+++ src/main/resources/static/js/project/details/details.js
@@ -15,11 +15,13 @@ layui.config({
         shade: [0.3, '#333']
     });
 
+    var payIndex; //收款码弹框下标
+    let index;  //预定表单界面 下标
+    var playViewIndex; //上传支付截图照片 下标
 
     //获取其他页传过来的房间id
     let localDate = layui.sessionData('roomInfoData');
     let roomId = localDate.roomId;
-    console.info("房间Id：" + roomId);
     if (roomId == null) {
         return layer.msg("房间Id不存在");
     } else {
@@ -64,6 +66,7 @@ layui.config({
         $('#coverImg').attr("src", '/image/' + data.roomPhoto);
     }
 
+
     //预定酒店
     $('body').on('click', '#shopEvent', function () {
 
@@ -77,7 +80,7 @@ layui.config({
              })
          } else {
         $("#bookRoomName").val($("#roomName").html())
-        let index = layer.open({
+             index= layer.open({
             title: '会员预定房间'
             , type: 1
             , offset: 'auto'
@@ -107,22 +110,87 @@ layui.config({
         ,max:7
     });
 
-    $.ajaxSetup({
+  /*  $.ajaxSetup({
         // 发送cookie
         xhrFields: {
             withCredentials: true
         }
     });
+*/
+
 
 
     //监听提交
     form.on('submit(addHotelFloor)',function(data){
 
-        let loadIndex = layer.load(2, {
+      /*  let loadIndex = layer.load(2, {
             shade: [0.3, '#333']
+        });*/
+
+       var roomName = data.field.roomName;
+       var checkInDate = data.field.checkInDate;
+       var checkOutDate = data.field.checkOutDate;
+       //取出session中存的客户信息
+        let userData = layui.sessionData('userSession');
+        var realName = userData.user.realName;
+        var idCard = userData.user.idCard;
+        var userPhone =  userData.user.userPhone;
+        var roomTypeName = $("#roomTypeName").html();
+        console.log(roomName);
+        console.log(checkInDate);
+        console.log(checkOutDate);
+        console.log(realName);
+        console.log(idCard);
+        console.log(userPhone);
+        console.log(roomTypeName);
+
+        payIndex=layer.open({
+            type: 1
+            , content: $('#moneryReceiveCode')
+            ,height: 'full-110'
+            , title: '<p style="font-weight:bolder">请扫码付款</p>'
+            , offset: 'auto'
+            , area: ['400px', '620px']
+            , shade: 0
         });
 
-        $.post({
+
+        $("#btnEdit").click(function () {
+            console.log(payIndex);
+            layer.close(index);
+            layer.close(payIndex);
+            playViewIndex=layer.open({
+                type: 1
+                , content: $('#payView')
+                ,height: 'full-110'
+                , title: '<p style="font-weight:bolder">请上传支付截图照片 工作人员会尽快核实</p>'
+                , offset: 'auto'
+                , area: ['400px', '620px']
+                , shade: 0
+            })
+        });
+
+
+        //上传支付截图
+        upload.render({
+            elem: '#demo1'
+            , url: 'http://localhost:9001/bookOrder/uploadImg'
+            , xhrFields:{withCredentials: false}
+            , accept: "images"
+            , done: function (res) {
+                layer.msg('上传成功');
+                layui.$('#uploadDemoView').removeClass('layui-hide').find('img').attr('src', "/image/" + res.src);
+              /*  $("#roomPhoto").val(res.src);*/
+            }
+        });
+
+        $("#closePlayBut").click(function () {
+            layer.close(playViewIndex);
+        })
+
+
+
+       /* $.post({
             url: memberReserveHotelUrl,
             data:data.field,
             dataType: "json",
@@ -160,7 +228,7 @@ layui.config({
             error: function () {
 
             }
-        });
+        });*/
 
         return false;
     });
