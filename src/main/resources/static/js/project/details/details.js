a59011c28916210db40378e09e913ef2c432e8db
xuguangxing <1832377033@qq.com>
2021-01-05 09:28:14 +0800

    王菲

--- src/main/resources/static/js/project/details/details.js
+++ src/main/resources/static/js/project/details/details.js
@@ -29,7 +29,7 @@ layui.config({
         $.get({
             url: "http://localhost:9001/room/queryRoomById",
             data: {id: roomId},
-            xhrFields: {withCredentials: false},
+            hrFields: {withCredentials: false},
             success: function (res) {
                 layer.close(loadIndex);
                 initRoom(res);
@@ -67,23 +67,23 @@ layui.config({
     $('body').on('click', '#shopEvent', function () {
 
         let userData = layui.sessionData('userSession');
-         if (typeof (userData.user) == "undefined" || userData.user == null) {
-             layer.msg('请先登录', {
-                 icon: 2,
-                 time: 1000
-             }, function () {
-                 window.location.href = "/login/login";
-             })
-         } else {
-        $("#bookRoomName").val($("#roomName").html())
-             index= layer.open({
-            title: '预定房间'
-            , type: 1
-            , offset: 'auto'
-            , area: ['30%', '30%']
-            , content: $('#orderView')
-            , shade: 0 //不显示遮罩
-        });
+        if (typeof (userData.user) == "undefined" || userData.user == null) {
+            layer.msg('请先登录', {
+                icon: 2,
+                time: 1000
+            }, function () {
+                window.location.href = "/login/login";
+            })
+        } else {
+            $("#bookRoomName").val($("#roomName").html())
+            index = layer.open({
+                title: '预定房间'
+                , type: 1
+                , offset: 'auto'
+                , area: ['30%', '30%']
+                , content: $('#orderView')
+                , shade: 0 //不显示遮罩
+            });
         }
     });
 
@@ -94,22 +94,22 @@ layui.config({
     //入住时间选择
     laydate.render({
         elem: '#checkInDate'
-        ,min: 0
-        ,max:6
-        ,showBottom: false
+        , min: 0
+        , max: 6
+        , showBottom: false
     });
 
     //退房时间选择
     laydate.render({
         elem: '#checkOutDate'
-        ,min: 1
-        ,max: 7
-        ,showBottom: false
+        , min: 1
+        , max: 7
+        , showBottom: false
     });
 
 
     //监听提交
-    form.on('submit(addHotelFloor)',function(data) {
+    form.on('submit(addHotelFloor)', function (data) {
         var roomName = data.field.roomName;
         var checkInDate = data.field.checkInDate;
         var checkOutDate = data.field.checkOutDate;
@@ -122,40 +122,49 @@ layui.config({
         var roomMonery = $("#roomPrice").html();
 
         //计算总共住了多少天
-        var days=(Date.parse(checkOutDate)-Date.parse(checkInDate))/(1*24*60*60*1000);
-       var total_amount = days*roomMonery;
+        var days = (Date.parse(checkOutDate) - Date.parse(checkInDate)) / (1 * 24 * 60 * 60 * 1000);
+        var total_amount = days * roomMonery;
 
 
         //生成订单号
         var myDate = new Date();
-        var time="";
-        time=time+myDate.getFullYear();    //获取完整的年份(4位,1970-????)
-        time=time+myDate.getMonth();       //获取当前月份(0-11,0代表1月)
-        time=time+myDate.getDate();        //获取当前日(1-31)
-        time=time+myDate.getHours();       //获取当前小时数(0-23)
-        time=time+myDate.getMinutes();     //获取当前分钟数(0-59)
+        var time = "";
+        time = time + myDate.getFullYear();    //获取完整的年份(4位,1970-????)
+        time = time + myDate.getMonth();       //获取当前月份(0-11,0代表1月)
+        time = time + myDate.getDate();        //获取当前日(1-31)
+        time = time + myDate.getHours();       //获取当前小时数(0-23)
+        time = time + myDate.getMinutes();     //获取当前分钟数(0-59)
 
-        time=time+myDate.getSeconds();     //获取当前秒数(0-59)
+        time = time + myDate.getSeconds();     //获取当前秒数(0-59)
 
         //生成4位随机数
-        var a="";
-        for (var i=0;i<4;i++){
-            a=a+Math.ceil(Math.random()*10)+"";
+        var a = "";
+        for (var i = 0; i < 4; i++) {
+            a = a + Math.ceil(Math.random() * 10) + "";
         }
         // 获取从1000到10000的随机整数 ，取0的概率极小。
-        time = time+a;
+        time = time + a;
         //生成订单信息
-         $.ajax({
-             url: "http://localhost:9001/bookOrder/addBookOrder",
-             data: {"orderNum": time,"realName": realName,"idCard": idCard,"userPhone":userPhone,"arriveDate": checkInDate,
-             "leaveDate":checkOutDate,"roomTypeName":roomTypeName,"roomName": roomName,"sumMonery":total_amount},
-             success: function (obj) {
+        $.ajax({
+            url: "http://localhost:9001/bookOrder/addBookOrder",
+            data: {
+                "orderNum": time,
+                "realName": realName,
+                "idCard": idCard,
+                "userPhone": userPhone,
+                "arriveDate": checkInDate,
+                "leaveDate": checkOutDate,
+                "roomTypeName": roomTypeName,
+                "roomName": roomName,
+                "sumMonery": total_amount
+            },
+            success: function (obj) {
 
-             }
-         })
+            }
+        })
 
         /* 跳转到支付界面*/
-    //   location.href="http://localhost:9001/pay/payView?out_trade_no="+time+"&&subject="+roomName+"房间   单价："+roomMonery+"元一天&&body=共住"+days+"天&&total_amount="+total_amount;
+        location.href = "http://localhost:9001/pay/payView?out_trade_no=" + time + "&&subject=" + roomName + "房间   单价：" + roomMonery + "元一天&&body=共住" + days + "天&&total_amount=" + total_amount;
     });
     return false;
 })
--- src/main/resources/static/js/project/myReserve/myReserve.js
+++ src/main/resources/static/js/project/myReserve/myReserve.js
@@ -12,7 +12,11 @@ layui.config({
     var laydate = layui.laydate;
     var element = layui.element;
     var carousel = layui.carousel;
-    var device = layui.device();
+    var device = layui.device()
+    var laytpl = layui.laytpl;
+    var util = layui.util;
+    var flow = layui.flow;
+
     var userId = -1;  //代表没有登录
     let userData = layui.sessionData('userSession');
     if (userData.user != "undefined" && userData.user != null) {
@@ -31,7 +35,7 @@ layui.config({
                 , {title: '操作', minWidth: 120, templet: '#barDemo', align: "center"}
                 , {field: 'id', title: 'ID', hide: true, width: 80, unresize: true, sort: true, align: 'center'}
                 , {field: 'orderNum', title: '订单号', minWidth: 260, align: 'center'}
-                , {field: 'roomId', title: '房间号', width: 80, templet: '#roomIdTpl', align: 'center'}
+                , {field: 'roomName', title: '房间号', width: 80, templet: '#roomIdTpl', align: 'center'}
                 , {field: 'sumMonery', title: '总金额', width: 100, templet: '#amountMoneyTpl', align: 'center'}
                 , {
                 field: 'status', title: '订单状态', width: 140, align: 'center', templet: function (d) {
@@ -65,55 +69,77 @@ layui.config({
         , limit: 10
         , page: true
     };
-    /*取出session中存储的用户*/
-
     table.render(reserveTable);
-
-    /* table.reload('bookOrderTable', {
-         page: {
-             curr: 1 //重新从第 1 页开始
-         }, where: {
-             userId: userId
-         }
-     });*/
 
     //监听行工具事件
     table.on('tool(bookOrderTable)', function (obj) {
-        var data = obj.data;
+       var data = obj.data;
+        console.log(data);
         /*退款*/
-        if (obj.event === 'refundMonery'){
-            layer.confirm('确认要退款吗', function (index) {
-              /*  $.ajax({
-                    url: '/bookOrder/deleteBookOrder',
-                    data: {
-                        id: data.id,
-                        roomTypeName: data.roomTypeName,
-                        roomName: data.roomName
-                    },
-                    success: function (obj) {
-                        layer.msg("删除成功", {
-                            icon: 1,
-                            time: 1000
-                        }, function () {
-                            window.location.href = "/bookOrder/bookOrderView";
+        if (obj.event === 'refundMonery') {
+
+
+        }
+        if (obj.event === 'payMonery') {
+            $.ajaxSetup({
+                // 发送cookie
+                xhrFields: {
+                    withCredentials: false
+                }
+            });
+            $.get({
+                hrFields: {withCredentials: false},
+                url: "http://localhost:9001/room/queryRoomById",
+                data: {id: data.roomId},
+                success: function (res) {
+                    if (res.status == 0) {
+                        //可预订
+                        /* 跳转到支付界面*/
+                        //计算总共住了多少天
+                        var days = (Date.parse(data.leaveDate) - Date.parse(data.arriveDate)) / (1 * 24 * 60 * 60 * 1000);
+
+                        location.href = "http://localhost:9001/pay/payView?out_trade_no=" + toOrderNum() + "&&subject=" + data.roomName + "房间   单价：" + data.roomPrice + "元一天&&body=共住" + days + "天&&total_amount=" + data.sumMonery;
+                    } else {
+                        layer.msg("该房间已被使用", {
+                            icon: 2,
+                            time: 1000,
                         })
                     }
-                });*/
-                layer.close(index);
-            });
+                }
+            })
         }
     })
-
-
 })
 
+//生成订单号
+function toOrderNum() {
 
-    /*时间格式化*/
-    function timestampToTime(time) {
-        var date = new Date(time);
-        var Y = date.getFullYear() + '-';
-        var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-        var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
-        let strDate = Y + M + D;
-        return strDate;
+    var myDate = new Date();
+    var time = "";
+    time = time + myDate.getFullYear();    //获取完整的年份(4位,1970-????)
+    time = time + myDate.getMonth();       //获取当前月份(0-11,0代表1月)
+    time = time + myDate.getDate();        //获取当前日(1-31)
+    time = time + myDate.getHours();       //获取当前小时数(0-23)
+    time = time + myDate.getMinutes();     //获取当前分钟数(0-59)
+    time = time + myDate.getSeconds();     //获取当前秒数(0-59)
+
+    //生成4位随机数
+    var a = "";
+    for (var i = 0; i < 4; i++) {
+        a = a + Math.ceil(Math.random() * 10) + "";
     }
+    // 获取从1000到10000的随机整数 ，取0的概率极小。
+    time = time + a;
+    return time;
+}
+
+
+/*时间格式化*/
+function timestampToTime(time) {
+    var date = new Date(time);
+    var Y = date.getFullYear() + '-';
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+    var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+    let strDate = Y + M + D;
+    return strDate;
+}
